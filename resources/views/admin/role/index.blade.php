aab9613ad3143626443485cd835e0ce8abf1dd3c
demigod66 <180402104@student.umri.ac.id>
2022-07-27 05:19:10 +0700

    dfghv

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -17,10 +17,7 @@
                     <thead>
                         <tr>
                             <th width="10%">No</th>
-                            <th>Nama</th>
-                            <th>NIDN/NIM</th>
-                            <th>Email</th>
-                            <th>Role</th>
+                            <th>Nama Role</th>>
                             <th width="10%">Aksi</th>
                         </tr>
                     </thead>
@@ -34,35 +31,22 @@
 
 
 
-{{-- <script type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function(){
       table = $('#example2').DataTable({
         processing : true,
         serverside : true,
-        ajax : "{{ route('userrole.index') }}",
+        ajax : "{{ route('role.index') }}",
         columns: [
         {data: 'DT_RowIndex', name:'DT_RowIndex'},
         {data: 'name', name:'name'},
-        {data: 'serial_user', name:'serial_user'},
-        {data: 'email', name:'email'},
-        {data: 'role', name:'role'},
         {data: 'action', name: 'action', orderable: false, searchable: false},
         ],
         order: [[0, 'asc']]
       });
     })
 
-    function tambah(){
-		typeSave = 'tambah';
-		$('#id').val('');
-		$('#form').trigger("reset");
-		$('.help-block').empty();
-		$('#modal-form').modal('show');
-		$('.modal-title').text('Tambah Data Ruangan');
-		$('#foto-sebelumnya').hide();
-		$('#foto-preview').empty();
-	}
-</script> --}}
+</script>
 
 
 
