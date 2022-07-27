cf808e96ce9d69ef82f2ccdd1283ea826fdde4b3
demigod66 <180402104@student.umri.ac.id>
2022-07-27 07:28:18 +0700

    role done

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -56,13 +56,38 @@
 		$('.modal-title').text('Tambah Data Role');
 	}
 
+
+    function get(id) {
+            typeSave = 'update';
+            $.ajax({
+                url: "{{ url('admin/edit-role') }}" + "/" + id,
+                type: "GET",
+                dataType: "JSON",
+                success: function(data) {
+                    $('[name="id"]').val(data.id);
+                    $('[name="nama_role"]').val(data.name);
+                    $('#modal-form').modal('show');
+                    $('.modal-title').text('Edit Data Role');
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    swal({
+                        title: 'Terjadi kesalahan',
+                        type: 'error',
+                        allowOutsideClick: false,
+                        allowEscapeKey: false,
+                        allowEnterKey: false,
+                    });
+                }
+            });
+        }
+
     function simpan() {
             var url;
             var id = $('#id').val();
             if (typeSave == 'tambah') {
                 url = "{{ route('role.simpan') }}";
             } else {
-                url = "{{ url('admin/produk/update') }}" + "/" + id;
+                url = "{{ url('admin/update-role') }}" + "/" + id;
             }
             $.ajax({
                 url: url,
@@ -97,6 +122,57 @@
         function reload() {
             table.ajax.reload(null, false);
         }
+
+        function hapus(id) {
+            swal({
+                title: 'Apakah kamu yakin?',
+                type: 'warning',
+                showCancelButton: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                confirmButtonText: 'Ya',
+                cancelButtonText: 'Tidak',
+                buttons: true
+            }).then(function() {
+                $.ajax({
+                    url: "{{ url('admin/hapus-role') }}" + "/" + id,
+                    type: "get",
+                    dataType: "JSON",
+                    success: function() {
+                        swal({
+                            title: 'Berhasil',
+                            type: 'success',
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            allowEnterKey: false,
+                        }).then(function() {
+                            reload();
+                        })
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        swal({
+                            title: 'Terjadi kesalahan',
+                            type: 'error',
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            allowEnterKey: false,
+                        });
+                    }
+                });
+            }, function(dismiss) {
+                if (dismiss === 'cancel') {
+                    swal({
+                        title: 'Batal',
+                        type: 'error',
+                        allowOutsideClick: false,
+                        allowEscapeKey: false,
+                        allowEnterKey: false,
+                    })
+                }
+            });
+        }
+    </script>
 
 
 </script>
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,9 @@ Route::group(['middleware' => 'auth', 'role:admin'], function () {
 
     Route::get('/admin/manajemen-role','RoleController@index')->name('role.index');
     Route::post('/admin/simpan-role','RoleController@simpan')->name('role.simpan');
+    Route::get('/admin/edit-role/{id}','RoleController@edit')->name('role.edit');
+    Route::post('/admin/update-role/{id}','RoleController@update')->name('role.update');
+    Route::get('/admin/hapus-role/{id}','RoleController@hapus')->name('role.hapus');
 
 
 
